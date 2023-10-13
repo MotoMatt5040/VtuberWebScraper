258933743f891bfe5c76b6890b1e45215cc07d3e
motomatt5040 <motomatt5040@gmail.com>
2023-10-12 23:40:20 -0500

    changed link for spider start page to ALL VTUBERS section

--- vtuberscraper/vtuberscraper/spiders/vtuberspider.py
+++ vtuberscraper/vtuberscraper/spiders/vtuberspider.py
@@ -4,7 +4,7 @@ import scrapy
 class VtuberspiderSpider(scrapy.Spider):
     name = "vtuberspider"
     allowed_domains = ["virtualyoutuber.fandom.com"]
-    start_urls = ["https://virtualyoutuber.fandom.com/wiki/NIJISANJI"]
+    start_urls = ["https://virtualyoutuber.fandom.com/wiki/Special:AllPages"]
 
     def parse(self, response):
         pass
