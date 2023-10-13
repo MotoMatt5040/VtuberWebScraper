04176b5793023571fe158e3a2cac5624c1485574
MotoMatt5040 <104686636+MotoMatt5040@users.noreply.github.com>
2023-10-12 22:35:16 -0500

    layout to store data

---
+++ main.py
@@ -0,0 +1,32 @@
+"""Sraper using scrapy"""
+'''
+vtubers_list = {
+    <name>: {
+        "media_links": {
+            "youtube": {
+                "main": <main_youtube_link>,
+                "clips: <clips_youtube_link>
+            },
+            "twitter": {
+                "main": <main_twitter_link>,
+                "nsfw": <nsfw_twitter_link>
+            },
+            "tiktok": <tiktok_link>,
+            "twitch": <twitch_link>,
+            "reddit": <reddit_link>,
+            "instagram": <instagram_link>
+        },
+        "images": {
+            "full body": [
+                <images>
+            ],
+            "portrait": [
+                <images>
+            ],
+            "outfits": [
+                <images>
+            ]
+        }  
+    }
+}
+'''
