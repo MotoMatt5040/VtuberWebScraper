d51dd2d3e61989c61f658386e8b88becdae07b35
motomatt5040 <motomatt5040@gmail.com>
2023-10-15 21:55:51 -0500

    spider can now crawl all the list pages without error

--- vtuberscraper/vtuberscraper/spiders/vtuberspider.py
+++ vtuberscraper/vtuberscraper/spiders/vtuberspider.py
@@ -1,16 +1,30 @@
 import scrapy
+import logging
+
+logging.getLogger('scrapy').propegate = False
 
 
 class VtuberspiderSpider(scrapy.Spider):
     name = "vtuberspider"
     allowed_domains = ["virtualyoutuber.fandom.com"]
     start_urls = ["https://virtualyoutuber.fandom.com/wiki/Special:AllPages"]
+    first = True
 
     def parse(self, response):
         data = response.css('div.mw-allpages-body li a')
-        next_page = response.css('div.mw-allpages-nav a ::attr(href)').get()
+
         for item in data:
             yield {
                 'name': item.css('a::text').get(),
                 'url': item.css('a').attrib['href']
             }
+
+        if not self.first:
+            next_page = response.css('div.mw-allpages-nav a ::attr(href)')[1].get()
+        else:
+            next_page = response.css('div.mw-allpages-nav a ::attr(href)').get()
+            self.first = False
+
+        if next_page is not None:
+            next_page_url = f'https://virtualyoutuber.fandom.com{next_page}'
+            yield response.follow(next_page_url, callback=self.parse)
