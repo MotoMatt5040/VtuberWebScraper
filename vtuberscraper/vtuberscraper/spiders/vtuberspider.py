2b73b5c22cf8a2f47f6309d7ee686f7a0bb80d73
motomatt5040 <motomatt5040@gmail.com>
2023-10-16 21:36:36 -0500

    Added finalized layout for current data structure

--- vtuberscraper/vtuberscraper/spiders/vtuberspider.py
+++ vtuberscraper/vtuberscraper/spiders/vtuberspider.py
@@ -41,16 +41,17 @@ class VtuberspiderSpider(scrapy.Spider):
 
         for item in data:
            yield {
-               "name": item.css('aside.portable-infobox h2::text')[0].get(),
-               "agency": item.xpath(
-                   '//div[@data-source="affiliation"]//div[@class="pi-data-value pi-font"]//a/text()').get(),
-               "media": media,
-               'gender': data.xpath('//div[@data-source="gender"]//div/text()').get(),
-               'age': data.xpath('//div[@data-source="age"]//div/text()').get(),
-               'birthday': data.xpath('//div[@data-source="birthday"]//div/text()').get(),
-               'height': data.xpath('//div[@data-source="height"]//div/text()').get(),
-               'zodiac': data.xpath('//div[@data-source="zodiac"]//div/text()').get(),
-               'emoji': data.xpath('//div[@data-source="emoji"]//div/text()').get(),
+               item.css('aside.portable-infobox h2::text')[0].get(): {
+                   "agency": item.xpath(
+                       '//div[@data-source="affiliation"]//div[@class="pi-data-value pi-font"]//a/text()').get(),
+                   "media": media,
+                   'gender': data.xpath('//div[@data-source="gender"]//div/text()').get(),
+                   'age': data.xpath('//div[@data-source="age"]//div/text()').get(),
+                   'birthday': data.xpath('//div[@data-source="birthday"]//div/text()').get(),
+                   'height': data.xpath('//div[@data-source="height"]//div/text()').get(),
+                   'zodiac': data.xpath('//div[@data-source="zodiac"]//div/text()').get(),
+                   'emoji': data.xpath('//div[@data-source="emoji"]//div/text()').get(),
+               }
            }
 
         while True:
@@ -63,17 +64,12 @@ class VtuberspiderSpider(scrapy.Spider):
                 continue
             break
 
-
         if next_page is not None:
             next_page_url = f'https://virtualyoutuber.fandom.com/wiki/{encode_url}'
             print('\n\n\n')
             print(next_page_url)
             print('\n\n\n')
             yield response.follow(next_page_url, callback=self.parse)
-
-
-
-
 
     @staticmethod
     def pull_media(data):
@@ -85,17 +81,21 @@ class VtuberspiderSpider(scrapy.Spider):
             'social links': {},
             'official websites': {}
         }
+
         i = 0
         for item in channel_links:
             media['channels'][item.get()] = data.xpath('//div[@data-source="channel"]//a/@href')[i].get()
             i += 1
+
         i = 0
         for item in social_links:
             media['social links'][item.get()] = data.xpath('//div[@data-source="social_media"]//a/@href')[i].get()
             i += 1
+
         i = 0
         for item in official_website_links:
             media['official websites'][item.get()] = data.xpath('//div[@data-source="official_website"]//a/@href')[
                 i].get()
             i += 1
+
         return media
